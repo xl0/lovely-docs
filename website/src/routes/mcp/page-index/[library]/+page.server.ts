ab415905028fd67e4b5d7bd42b4cf4c10fbc5d94
Alexey Zaytsev <alexey.zaytsev@gmail.com>
2025-11-22 04:42:21 -0500

    Clean up mcp resource pages

--- website/src/routes/mcp/page-index/[library]/+page.server.ts
+++ website/src/routes/mcp/page-index/[library]/+page.server.ts
@@ -1,14 +1,14 @@
-import type { PageServerLoad } from './$types';
+// import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ parent, params }) => {
-	const parentData = await parent();
-	const library = params.library;
-	const pageIndex = parentData.mcp.pageIndexes.find((p: any) => p.label === library);
+// export const load: PageServerLoad = async ({ parent, params }) => {
+// 	const parentData = await parent();
+// 	const library = params.library;
+// 	const pageIndex = parentData.mcp.pageIndexes.find((p: any) => p.label === library);
 
-	return {
-		...parentData,
-		library,
-		tree: pageIndex?.tree,
-		hasTree: !!pageIndex?.tree
-	};
-};
+// 	return {
+// 		...parentData,
+// 		library,
+// 		tree: pageIndex?.tree,
+// 		hasTree: !!pageIndex?.tree
+// 	};
+// };
